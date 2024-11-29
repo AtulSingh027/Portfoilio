9313c7aeaa8416316e359111ec2f6f8dd5a9d308
deepak-vishwakarma <vishwakarmad2337@gmail.com>
2024-11-29 16:40:15 +0530

    my portpholiyo

--- src/Component/Navigation.jsx
+++ src/Component/Navigation.jsx
@@ -16,7 +16,7 @@ export default function Navigation() {
                 <li><Link to ="/" >HOME</Link></li>
                 <li><Link to ="/Project">PROJECTS</Link></li>
                 <li><Link to ="/Skills">SKILLS</Link></li>
-                <li><Link to ="/Contect">CONTACT</Link></li>
+                <li><Link to ="/Contact">CONTACT</Link></li>
                 <li><Link to ="/Education">EDUCATION</Link></li>
             </ul>
 
