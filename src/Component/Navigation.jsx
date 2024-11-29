25504f278e408d73233229db88ddddaae93d7c3a
deepak-vishwakarma <vishwakarmad2337@gmail.com>
2024-11-29 15:05:49 +0530

    my peotfolio

---
+++ src/Component/Navigation.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Navigation.css"
+import { Link } from 'react-router-dom'
+
+export default function Navigation() {
+  return (
+    <div className='main-navigation'>
+      
+        <div className='brand-container'>
+            <h1>Logo</h1>
+        </div>
+
+        <div className='navigation-menu'>
+
+            <ul className='navigation-links'>
+                <li><Link to ="/" >HOME</Link></li>
+                <li><Link to ="/Project">PROJECTS</Link></li>
+                <li><Link to ="/Skills">SKILLS</Link></li>
+                <li><Link to ="/Contect">CONTACT</Link></li>
+                <li><Link to ="/Education">EDUCATION</Link></li>
+            </ul>
+
+        </div>
+    </div>
+  ) 
+}
